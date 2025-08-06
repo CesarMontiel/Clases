356492ff1caa887440ef532d0e80cdf640783830
CesarMontiel <cmontielmoctezuma@gmail.com>
2025-08-05 21:50:04 -0600

    uhjhnlkjlkacnjlsdhpidmalkjdnalsjncljwnlkqjnedlkjqneldjnasldkjcnalsdkjnclqjk4nr1lkj4n3l1kdjnlkjandlckjnwelkj1n4l3kjfn1lkjnlkajsndlkjwendlwqkjñoiñkmñomñlkm432oikr´p34okr234rk23p4okr2´34pork2´34ork23op4kr2po34kr2po3kr234pokrdf,snsdfjn

--- Desarrollo Web/Ejercicio 4/programacion_declarativa.js
+++ Desarrollo Web/Ejercicio 4/programacion_declarativa.js
@@ -1,5 +1,9 @@
 const numeros = [1, 2, 3, 4, 5, 6];
 
+const evalPar = n => n % 2 === 0;
+
+console.log("Resultado:", evalPar(54 )); 
+
 const resultado = numeros.filter(n => n % 2 === 0).map(n => n * 2);
 
 console.log(resultado); // [4, 8, 12]
